6aa820e4f911b434aeeb1128c7a16aa699d4b8a5
ryanz2k <johnryangomez812@gmail.com>
2023-05-11 23:28:00 +0800

    updated file 5/11

--- Documents/NetBeansProjects/Inventory Management System and POS/src/IMS/stocks.java
+++ Documents/NetBeansProjects/Inventory Management System and POS/src/IMS/stocks.java
@@ -228,7 +228,7 @@ public class stocks extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 997, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
